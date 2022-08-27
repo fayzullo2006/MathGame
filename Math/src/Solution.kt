da6ca01cf1ee4e6aafcd5b4fe3d34ca964dee1a3
Fayzullo <110022607+fayzullo2006@users.noreply.github.com>
2022-08-26 22:32:51 -0400

    Add files via upload

---
+++ Math/src/Solution.kt
@@ -0,0 +1,16 @@
+
+    class Solution {
+        var name: String? = null
+        var t = 0
+        var x = 0
+        var res = 0
+
+
+
+        constructor() {}
+        constructor(name: String?, t: Int, x: Int) {
+            this.name = name
+            this.t = t
+            this.x = x
+        }
+    }
